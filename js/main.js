26a88adfe6de24b544355b278e741c153505df69
kapreshka <anarbekova.kanykei20@gmail.com>
2022-03-31 13:14:12 +0600

    first

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// console.log('Hello World!');
+
+
+// let example = 'Hello';
+// const example2 = 'Hi';
+
+// console.log(example, example2);
+
+// let name = 'Kani';
+// let name = 'Makers';
+// console.log(name);
+
+// const name = 'Kani';
+// // name = 'Makers';
+// console.log(name);
+
+// STRING
+// let str = 'Hello World';
+// let str1 = 'Hi';
+// console.log(str, str1);
+
+
+// NUMBER
+// let num = 25;
+// let num1 = 33;
+// console.log(num, num1);
+
+// Boolean
+
+// let bool1 = true;
+// let bool2 = false;
+// console.log(bool1, bool2);
+
+// UNDERFINED
+
+// let a 
+// console.log(a);
+
+// null
+// let b = null;
+// console.log(b);
+
+// Object
+// let obj = {name: 'Kani',
+// lastName: 'dddddffdf',
+//  age: 22, 
+//  hobby: null, 
+//  isHuman: true,
+//  dog: {
+//     name: 'Oreo',
+//     color: 'white'
+//  }
+// }
+// console.log(obj);
+// console.log(obj.name);
+// console.log(obj.lastName);
+
+let arr = [1, 2, 3, 4, true, false, null, 'hello', {name: 'Kani'}];
+console.log(arr);
+console.log(arr[4]);
+console.log(arr.length);
+console.log(arr.length - 1);
